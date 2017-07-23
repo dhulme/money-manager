78bf062c2aa0fc7f135264f28be3aadc3b4a0eda
David Hulme <dhulme@outlook.com>
2017-07-23 18:34:44 +0100

    Router and vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+
+Vue.use(Vuex);
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
