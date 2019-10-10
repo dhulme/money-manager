992abc3b52e23ef480684d295b6473d9e8b2626c
Dave Hulme <David.Hulme@mitie.com>
2019-10-10 16:54:35 +0100

    Testing new hooks

--- src/renderer/components/pages/BulkTransaction.vue
+++ src/renderer/components/pages/BulkTransaction.vue
@@ -1,11 +1,20 @@
 <template>
-  <div v-hotkey.close="goToBulkTransactionsIfDialogClosed" v-hotkey.add="addTransaction">
+  <div
+    v-hotkey.close="goToBulkTransactionsIfDialogClosed"
+    v-hotkey.add="addTransaction"
+  >
     <VCard>
       <VCardTitle>
         <span class="headline">{{ bulkTransaction.name }}</span>
         <VBtn text @click="addTransaction">Add</VBtn>
         <VSpacer />
-        <VTextField v-model="search" append-icon="search" label="Search" single-line hide-details />
+        <VTextField
+          v-model="search"
+          append-icon="search"
+          label="Search"
+          single-line
+          hide-details
+        />
       </VCardTitle>
       <VSubheader>{{ bulkTransaction.description }}</VSubheader>
       <BulkTransactionTransactions
@@ -31,91 +40,91 @@
 </template>
 
 <script>
-  import moment from 'moment';
+import moment from 'moment';
 
-  import util from '../../util';
+import util from '../../util';
 
-  import BulkTransactionTransactions from '../BulkTransactionTransactions.vue';
+import BulkTransactionTransactions from '../BulkTransactionTransactions.vue';
 import BulkTransactionEdit from '../BulkTransactionEdit.vue';
 
-  export default {
-    components: {
-      BulkTransactionTransactions,
-      BulkTransactionEdit
-    },
-    data() {
-      return {
-        search: '',
-        dialogVisible: false,
-        transaction: {},
-        transactions: []
-      };
-    },
-    computed: {
-      bulkTransaction() {
-        return this.$store.getters['project/bulkTransaction'](
-          this.$route.params.bulkTransactionId
-        );
-      }
-    },
-    created() {
-      this.transactions = this.$store.getters[
-        'project/bulkTransactionTransactions'
-      ](this.bulkTransaction);
-      this.$ipc.setTitle(this.bulkTransaction.name);
-    },
-    methods: {
-      process() {
-        this.$store.dispatch('project/runBulkTransactionTransactions', {
-          bulkTransaction: this.bulkTransaction,
-          transactions: this.transactions.map(transaction => ({
-            ...transaction,
-            id: util.getId(),
-            date: moment()
-          }))
-        });
-        this.$store.dispatch('openSnackbar', 'Transactions done');
-      },
-      editTransaction(transaction) {
-        this.transaction = transaction;
-        this.dialogVisible = true;
-      },
-      addTransaction() {
-        this.transaction = {};
-        this.dialogVisible = true;
-      },
-      savedTransaction(transaction) {
-        this.dialogVisible = false;
-        const index = this.transactions.findIndex(_ => _.id === transaction.id);
-        if (index !== -1) {
-          this.transactions.splice(index, 1, transaction);
-        } else {
-          this.transactions.push(transaction);
-        }
-      },
-      goToBulkTransactionsIfDialogClosed() {
-        if (!this.dialogVisible) {
-          this.$router.push({
-            name: 'bulkTransactions'
-          });
-        }
-      },
-      duplicate() {
-        this.$store.commit('setNewBulkTransaction', {
-          ...this.bulkTransaction,
-          transactions: this.transactions
-        });
-        this.$router.push({
-          name: 'newBulkTransaction'
-        });
-      }
+export default {
+  components: {
+    BulkTransactionTransactions,
+    BulkTransactionEdit
+  },
+  data() {
+    return {
+      search: '',
+      dialogVisible: false,
+      transaction: {},
+      transactions: []
+    };
+  },
+  computed: {
+    bulkTransaction() {
+      return this.$store.getters['project/bulkTransaction'](
+        this.$route.params.bulkTransactionId
+      );
     }
-  };
+  },
+  created() {
+    this.transactions = this.$store.getters[
+      'project/bulkTransactionTransactions'
+    ](this.bulkTransaction);
+    this.$ipc.setTitle(this.bulkTransaction.name);
+  },
+  methods: {
+    process() {
+      this.$store.dispatch('project/runBulkTransactionTransactions', {
+        bulkTransaction: this.bulkTransaction,
+        transactions: this.transactions.map(transaction => ({
+          ...transaction,
+          id: util.getId(),
+          date: moment()
+        }))
+      });
+      this.$store.dispatch('openSnackbar', 'Transactions done');
+    },
+    editTransaction(transaction) {
+      this.transaction = transaction;
+      this.dialogVisible = true;
+    },
+    addTransaction() {
+      this.transaction = {};
+      this.dialogVisible = true;
+    },
+    savedTransaction(transaction) {
+      this.dialogVisible = false;
+      const index = this.transactions.findIndex(_ => _.id === transaction.id);
+      if (index !== -1) {
+        this.transactions.splice(index, 1, transaction);
+      } else {
+        this.transactions.push(transaction);
+      }
+    },
+    goToBulkTransactionsIfDialogClosed() {
+      if (!this.dialogVisible) {
+        this.$router.push({
+          name: 'bulkTransactions'
+        });
+      }
+    },
+    duplicate() {
+      this.$store.commit('setNewBulkTransaction', {
+        ...this.bulkTransaction,
+        transactions: this.transactions
+      });
+      this.$router.push({
+        name: 'newBulkTransaction'
+      });
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  header {
-    display: flex;
-    justify-content: space-between;
-  }
+header {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
